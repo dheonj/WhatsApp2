81fc9883b78338f4a72195ec82eca6176701344c
jdh <dhdhdh.j@gmail.com>
2022-11-22 10:55:33 +0900

    some tests about importing components and functions

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from './src/components/Test'
+// any name when importing a component (with a capital letter) 
+// because the source exports by default
+import index from './src/components/Test'
+// if you 
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>haha</Text>
+        <Test/>         
+        {/* import a component  */}
+        {(index)()} 
+        {/* import a function and use iife */}
       <StatusBar style="auto" />
     </View>
   );
